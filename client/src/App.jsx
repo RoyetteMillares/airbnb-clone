205f2c4175ad01f5291132d1155b4a7aef6e5efe
royetteM <royettezxc@proton.me>
2023-03-29 18:32:15 +0800

    hays

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,7 @@ const App = () => {
          <div>Any week</div>
          <div className="h-5 border-l border-gray-300"></div>
          <div>Add guests</div>
-         <button className='bg-primary text-white rounded-full h-8 w-8 flex justify-center items-center'>
+         <button className="bg-primary text-white rounded-full h-8 w-8 flex justify-center items-center">
            <svg
              xmlns="http://www.w3.org/2000/svg"
              fill="none"
@@ -43,6 +43,38 @@ const App = () => {
              />
            </svg>
          </button>
+       </div>
+       <div className="flex gap-2 items-center px-4 py-2 border border-gray-300 rounded-full shadow-sm shadow-gray-300">
+         <svg
+           xmlns="http://www.w3.org/2000/svg"
+           fill="none"
+           viewBox="0 0 24 24"
+           strokeWidth={1.5}
+           stroke="currentColor"
+           className="w-6 h-6"
+         >
+           <path
+             strokeLinecap="round"
+             strokeLinejoin="round"
+             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+           />
+         </svg>
+         <div className="bg-gray-500 text-white rounded-full p-1">
+           <svg
+             xmlns="http://www.w3.org/2000/svg"
+             fill="none"
+             viewBox="0 0 24 24"
+             strokeWidth={1.5}
+             stroke="currentColor"
+             className="w-6 h-6"
+           >
+             <path
+               strokeLinecap="round"
+               strokeLinejoin="round"
+               d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+             />
+           </svg>
+         </div>
        </div>
      </header>
    </React.Fragment>
